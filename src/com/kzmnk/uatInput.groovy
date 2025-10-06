7e95f21eff2b6b92722b996531f28cf0c3acf3ce
itsoktoxcl <itsok2excl@gmail.com>
2025-10-06 17:05:15 +0100

    Create uatInput.groovy

---
+++ src/com/kzmnk/uatInput.groovy
@@ -0,0 +1,16 @@
+package com.kzmnk
+
+public class uatInput {
+
+  def buildIsUatApproved(){
+    def file = new File("/_tools/approved.txt")
+
+    if (file.exists()){
+      return true;
+    }
+    else {
+      println "Approval file does not exist."
+    }
+    return false;
+  }
+}
